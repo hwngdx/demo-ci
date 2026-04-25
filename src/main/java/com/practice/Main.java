a33d1a1c67bd794ad630737f930f065094734a97
Hung Do <dogiahung3k1@gmail.com>
2026-04-26 05:02:34 +0700

    add checkstyle

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -1,14 +1,15 @@
 package com.practice;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-  static void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+  public static void main(String[] args) {
+    System.out.println("Hello World");
 
-    int abc_aaa = 100;
-    System.out.println(abc_aaa);
+    // Lỗi 1: Tên biến không theo chuẩn camelCase (có dấu gạch dưới)
+    int so_thu_nhat = 10;
+
+    // Lỗi 2: Thiếu khoảng trắng sau 'if' (chuẩn Google bắt buộc có dấu cách)
+    if(so_thu_nhat > 0) {
+      System.out.println(so_thu_nhat);
+    }
   }
 }
