10ff4db54450d2f345449ac5b143c629b9135e76
Hung Do <dogiahung3k1@gmail.com>
2026-04-26 04:41:24 +0700

    test checkstyle

---
+++ src/main/java/com/practice/Main.java
@@ -0,0 +1,15 @@
+package com.practice;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  static void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+
+    int aB = 100;
+    System.out.println(aB);
+
+  }
+}
