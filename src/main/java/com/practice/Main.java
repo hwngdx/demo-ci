7be7763775154e3d8e736e11ee2a0d04e4935425
Hung Do <dogiahung3k1@gmail.com>
2026-04-26 16:11:06 +0700

    checkstyle

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -2,6 +2,9 @@ package com.practice;
 
 public class Main {
   public static void main(String[] args) {
+
+    final int abc = 1;
+
     System.out.println("Hello World");
 
     int so_thu_nhat = 10;
