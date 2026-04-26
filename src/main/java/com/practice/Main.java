1311284968db805c8b8363614068abde6a6b72f9
Hung Do <dogiahung3k1@gmail.com>
2026-04-26 13:59:32 +0700

    test checkstyle

--- src/main/java/com/practice/Main.java
+++ src/main/java/com/practice/Main.java
@@ -4,10 +4,8 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World");
 
-    // Lỗi 1: Tên biến không theo chuẩn camelCase (có dấu gạch dưới)
     int so_thu_nhat = 10;
 
-    // Lỗi 2: Thiếu khoảng trắng sau 'if' (chuẩn Google bắt buộc có dấu cách)
     if(so_thu_nhat > 0) {
       System.out.println(so_thu_nhat);
     }
